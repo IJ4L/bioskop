8fa9b8d50528a5d94ad47c4ca4be9f34a3059fd9
Nursyamsu Rijal Usman <119686454+IJ4L@users.noreply.github.com>
2023-11-07 00:19:21 +0800

    update: db

--- config/database.go
+++ config/database.go
@@ -6,7 +6,7 @@ import (
 )
 
 func InitDatabase() *gorm.DB {
-	dsn := "root:@tcp(127.0.0.1:3306)/cinema_ticket?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:Ijalganteng1@@tcp(103.175.219.55:3306)/cinema_ticket?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
